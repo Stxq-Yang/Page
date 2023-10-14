06609bac3284bd45c575be0934d50868d8fabbd4
Stxq-Yang <84013188+Stxq-Yang@users.noreply.github.com>
2023-10-14 09:42:50 +0800

    Update and rename OrbitControls[1].js to includes/OrbitControls.js

--- includes/OrbitControls.js
+++ includes/OrbitControls.js
@@ -1,15 +1,3 @@
-import {
-	EventDispatcher,
-	MOUSE,
-	Quaternion,
-	Spherical,
-	TOUCH,
-	Vector2,
-	Vector3,
-	Plane,
-	Ray,
-	MathUtils
-} from 'three';
 
 // OrbitControls performs orbiting, dollying (zooming), and panning.
 // Unlike TrackballControls, it maintains the "up" direction object.up (+Y by default).
